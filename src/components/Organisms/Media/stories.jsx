26c4e6926fc513878473fb8f48e97632a546d942
Danny Arntz <arntz.D@gmail.com>
2019-06-26 21:55:16 +0200

    remove excessive stories

--- src/components/Organisms/Media/stories.jsx
+++ src/components/Organisms/Media/stories.jsx
@@ -6,28 +6,6 @@ import Media from 'components/Organisms/Media';
 import data from './data.json';
 
 storiesOf('components|organisms.ContentComponents', module)
-  .add('Media without video', () => (
-    <Media
-      {...data}
-      vimeoId={null}
-      youtubeId={null}
-      videoDuration={null}
-      smallImage={data.smallImageIfNoVideo}
-      isNarrow={boolean('Is narrow', false)}
-      topBackground={select('Top background', {
-        white: 'white',
-        'gray-b': 'gray-b',
-        black: 'black',
-      },
-      'white')}
-      bottomBackground={select('Bottom background', {
-        white: 'white',
-        'gray-b': 'gray-b',
-        black: 'black',
-      },
-      'black')}
-    />
-  ))
   .add('Media with video', () => (
     <Media
       {...data}
